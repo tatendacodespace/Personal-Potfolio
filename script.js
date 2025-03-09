976f769e1403b12efc9a27e8c9474d0f21375087
Tatenda Midzi <tatendamidzi49@gmail.com>
2025-03-09 13:21:50 +0000

    updated javascript for hamburger menu

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
-    const menu = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
